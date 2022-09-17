ff5eaf21424331566be4273083cb159c292ba30a
shehjad97 <shehjadcse@gmail.com>
2022-09-17 21:35:48 +0600

    removed unnecessary codes

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ gpus= tf.config.list_physical_devices('GPU')
 if gpus:
     try:
         tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
-    except RunTimeError as e:
-        print(e)
+    except:
+        print("Error")
 
 # Load pipeline config and build a detection model
 configs = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
@@ -19,7 +19,8 @@ detection_model = model_builder.build(model_config=configs['model'], is_training
 
 # Restore checkpoint
 ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
-ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-11')).expect_partial()
+# ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-11')).expect_partial()
+ckpt.restore("files/ckpt-11.index").expect_partial()
 
 @tf.function
 def detect_fn(image):
@@ -31,72 +32,12 @@ def detect_fn(image):
 import cv2 
 import numpy as np
 from matplotlib import pyplot as plt
-# %matplotlib inline
 
 category_index = label_map_util.create_category_index_from_labelmap(files['LABELMAP'])
-
-# IMAGE_PATH = os.path.join(paths['IMAGE_PATH'], 'test', 'Cars411.png')
-
-# img = cv2.imread(IMAGE_PATH)
-# image_np = np.array(img)
-
-# input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
-# detections = detect_fn(input_tensor)
-
-# num_detections = int(detections.pop('num_detections'))
-# detections = {key: value[0, :num_detections].numpy()
-#               for key, value in detections.items()}
-# detections['num_detections'] = num_detections
-
-# # detection_classes should be ints.
-# detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
-
-# label_id_offset = 1
-# image_np_with_detections = image_np.copy()
-
-# viz_utils.visualize_boxes_and_labels_on_image_array(
-#             image_np_with_detections,
-#             detections['detection_boxes'],
-#             detections['detection_classes']+label_id_offset,
-#             detections['detection_scores'],
-#             category_index,
-#             use_normalized_coordinates=True,
-#             max_boxes_to_draw=5,
-#             min_score_thresh=.8,
-#             agnostic_mode=False)
-
-# plt.imshow(cv2.cvtColor(image_np_with_detections, cv2.COLOR_BGR2RGB))
-# plt.show()
 
 import easyocr
 
 detection_threshold = 0.7
-
-# image = image_np_with_detections
-# scores = list(filter(lambda x: x> detection_threshold, detections['detection_scores']))
-# boxes = detections['detection_boxes'][:len(scores)]
-# classes = detections['detection_classes'][:len(scores)]
-
-# width = image.shape[1]
-# height = image.shape[0]
-
-
-# # Apply ROI filtering and OCR
-# for idx, box in enumerate(boxes):
-#     print(box)
-#     roi = box*[height, width, height, width]
-#     print(roi)
-#     region = image[int(roi[0]):int(roi[2]),int(roi[1]):int(roi[3])]
-#     reader = easyocr.Reader(['en'])
-#     ocr_result = reader.readtext(region)
-#     print(ocr_result)
-#     plt.imshow(cv2.cvtColor(region, cv2.COLOR_BGR2RGB))
-
-# for result in ocr_result:
-#     print(np.sum(np.subtract(result[0][2],result[0][1])))
-#     print(result[1])
-
-# region_threshold = 0.05
 
 def filter_text(region, ocr_result, region_threshold):
     rectangle_size = region.shape[0]*region.shape[1]
@@ -109,8 +50,6 @@ def filter_text(region, ocr_result, region_threshold):
         if length*height / rectangle_size > region_threshold:
             plate.append(result[1])
     return plate
-
-# filter_text(region, ocr_result, region_threshold)
 
 region_threshold = 0.6
 
@@ -139,8 +78,6 @@ def ocr_it(image, detections, detection_threshold, region_threshold):
         print(text)
         return text, region
 
-# text, region = ocr_it(image_np_with_detections, detections, detection_threshold, region_threshold)
-
 import csv
 import uuid
 
@@ -153,12 +90,9 @@ def save_results(text, region, csv_filename, folder_path):
         csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([img_name, text])
 
-# save_results(text, region, 'detection_results.csv', 'Detection_Images')
-
 cap = cv2.VideoCapture(0)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
 
 while cap.isOpened(): 
     ret, frame = cap.read()
@@ -200,4 +134,4 @@ while cap.isOpened():
     if cv2.waitKey(10) & 0xFF == ord('q'):
         cap.release()
         cv2.destroyAllWindows()
-        break   
+        break
