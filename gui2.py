6ed4ed91deb1222771b46e3badd0e6e6f6471b72
ta6345538 <ta6345538@gmail.com>
2022-09-18 21:02:49 +0600

    integrated initial ui

--- gui2.py
+++ gui2.py
@@ -3,13 +3,16 @@ import numpy as np
 from tkinter import *
 from PIL import Image, ImageTk
 
+from main import webcam
+
 root = Tk()
 root.title('ETMS - Expert Traffic Manangement System')
 root.geometry("780x648")
 Label(root, text="ETMS_CAM01").pack() 
 
 def button_clicked():
-    print('Button clicked')
+    # print('Button clicked')
+    webcam()
 
 button = Button(root, text='Click Me', command=button_clicked)
 button.pack()
