84e3c66fc6c24f2c0762b197b145d1b03dd88432
ta6345538 <ta6345538@gmail.com>
2022-09-19 17:54:27 +0600

    added tab in gui4

---
+++ gui4.py
@@ -0,0 +1,30 @@
+import tkinter as tk					
+from tkinter import ttk
+from main import webcam
+
+root = tk.Tk()
+root.geometry("1200x1200")
+root.title("ETMS")
+tabControl = ttk.Notebook(root)
+
+tab1 = ttk.Frame(tabControl)
+tab2 = ttk.Frame(tabControl)
+tab3 = ttk.Frame(tabControl)
+
+tabControl.add(tab1, text ='Home')
+tabControl.add(tab2, text ='Data')
+tabControl.add(tab3, text ='Chart')
+tabControl.pack(expand = 1, fill ="both")
+
+def button_clicked():
+    # print('Button clicked')
+    webcam()
+
+button = tk.Button(tab1, text='Click Me', command=button_clicked)
+button.pack()
+
+
+# ttk.Label(tab1,text ="Welcome To Expert Traffic Management System").grid(column = 0,row = 0, padx = 30, pady = 30)
+ttk.Label(tab2,text ="Lets dive into the world of computers").grid(column = 0,row = 0,padx = 30, pady = 30)
+
+root.mainloop()
