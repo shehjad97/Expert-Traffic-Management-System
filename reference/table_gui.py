4406b618a64d008da92741529b60bfe93b34e8a7
shehjad97 <shehjadcse@gmail.com>
2022-09-19 18:03:51 +0600

    loaded json data on table_gui

--- reference/table_gui.py
+++ reference/table_gui.py
@@ -2,6 +2,8 @@
 from tkinter import *
 from tkinter import ttk
 
+import json
+
 # Create an instance of tkinter frame
 win = Tk()
 
@@ -13,19 +15,26 @@ style = ttk.Style()
 style.theme_use('clam')
 
 # Add a Treeview widget
-tree = ttk.Treeview(win, column=("FName", "LName", "Roll No"), show='headings', height=5)
+tree = ttk.Treeview(win, column=("Serial", "License Number", "Timestamp"), show='headings', height=15)
 tree.column("# 1", anchor=CENTER)
-tree.heading("# 1", text="FName")
+tree.heading("# 1", text="Serial")
 tree.column("# 2", anchor=CENTER)
-tree.heading("# 2", text="LName")
+tree.heading("# 2", text="License Number")
 tree.column("# 3", anchor=CENTER)
-tree.heading("# 3", text="Roll No")
+tree.heading("# 3", text="Timestamp")
 
-# Insert the data in Treeview widget
-tree.insert('', 'end', text="1", values=('Amit', 'Kumar', '17701'))
-tree.insert('', 'end', text="1", values=('Ankush', 'Mathur', '17702'))
-tree.insert('', 'end', text="1", values=('Manisha', 'Joshi', '17703'))
-tree.insert('', 'end', text="1", values=('Shivam', 'Mehrotra', '17704'))
+with open('json_data.json', 'r') as openfile:
+        records = json.load(openfile)
+
+for record in records:
+    # print(record['timestamp'])
+    tree.insert('', 'end', text="1", values=(record['no'], record['license_number'], record['timestamp']))
+
+# # Insert the data in Treeview widget
+# tree.insert('', 'end', text="1", values=('Amit', 'Kumar', '17701'))
+# tree.insert('', 'end', text="1", values=('Ankush', 'Mathur', '17702'))
+# tree.insert('', 'end', text="1", values=('Manisha', 'Joshi', '17703'))
+# tree.insert('', 'end', text="1", values=('Shivam', 'Mehrotra', '17704'))
 
 tree.pack()
 
