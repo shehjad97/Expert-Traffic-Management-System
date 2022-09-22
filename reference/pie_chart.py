3edc32ce5d69642bc55dc60b48cc8edc32eb0a95
shehjad97 <shehjadcse@gmail.com>
2022-09-22 23:57:48 +0600

    updated pie chart data

--- reference/pie_chart.py
+++ reference/pie_chart.py
@@ -6,7 +6,7 @@ import tkinter as tk
 fig = matplotlib.figure.Figure(figsize=(5,5))
 ax = fig.add_subplot(111)
 ax.pie([13,8,17,14,7,18,13,10]) 
-ax.legend(["DHA01 (13%)","DHA02 (8%)","DHA03 (17%)","DHA04 (14%)","DHA05 (7%)","DHA06 (18%)","DHA06 (13%)","DHA06 (10%)"])
+ax.legend(["DHA01 (13%)","DHA02 (8%)","DHA03 (17%)","DHA04 (14%)","DHA05 (7%)","DHA06 (18%)","DHA07 (13%)","DHA08 (10%)"])
 
 circle=matplotlib.patches.Circle( (0,0), 0.0, color='white')
 ax.add_artist(circle)
