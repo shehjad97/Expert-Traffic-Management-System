4a24311d94847fc42dbfe93f9225abcfbf6af54b
shehjad97 <shehjadcse@gmail.com>
2022-09-23 00:10:15 +0600

    changed legend style

--- reference/pie_chart.py
+++ reference/pie_chart.py
@@ -3,15 +3,16 @@ import matplotlib.patches
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
 
-fig = matplotlib.figure.Figure(figsize=(5,5))
+fig = matplotlib.figure.Figure(figsize=(5, 5))
 ax = fig.add_subplot(111)
-ax.pie([13,8,17,14,7,18,13,10]) 
-ax.legend(["DHA01 (13%)","DHA02 (8%)","DHA03 (17%)","DHA04 (14%)","DHA05 (7%)","DHA06 (18%)","DHA07 (13%)","DHA08 (10%)"])
 
-circle=matplotlib.patches.Circle( (0,0), 0.0, color='white')
+ax.pie([13, 8, 17, 14, 7, 18, 13, 10])
+ax.legend(["DHA01 (13%)", "DHA02 (8%)", "DHA03 (17%)", "DHA04 (14%)", "DHA05 (7%)", "DHA06 (18%)", "DHA07 (13%)", "DHA08 (10%)"], loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True, shadow=True)
+
+circle = matplotlib.patches.Circle((0, 0), 0.0, color='white')
 ax.add_artist(circle)
 
-window= tk.Tk()
+window = tk.Tk()
 canvas = FigureCanvasTkAgg(fig, master=window)
 canvas.get_tk_widget().pack()
 canvas.draw()
