dff892da9255586cf9eeb6f7c78f91c1633963f0
shehjad97 <shehjadcse@gmail.com>
2022-09-23 15:22:05 +0600

    aligned label texts in help.py

--- reference/help.py
+++ reference/help.py
@@ -11,13 +11,16 @@ style.theme_use('clam')
 
 parent = ttk.Frame(root)
 
+label_texts = """1. Click Start Detection To Run The System
+2. Click Vehicle Data To Show the Table of Detected Vehicle
+3. Click Yearly Data Chart To Show The Chart
+4. Click Monthly Data Chart To Show The Chart
+5. Click Cam Data Chart To Show The Chart
+"""
+
 # Labels
 ttk.Label(parent, text="Help",font=("Arial", 18)).pack(pady=25)
-ttk.Label(parent, text="1. Click Start Detection To Run The System",font=("Arial", 12)).pack(pady=5)
-ttk.Label(parent, text="2. Click Vehicle Data To Show the Table of Detected Vehicle",font=("Arial", 12)).pack(pady=5)
-ttk.Label(parent, text="3. Click Yearly Data Chart To Show The Chart",font=("Arial", 12)).pack(pady=5)
-ttk.Label(parent, text="4. Click Monthly Data Chart To Show The Chart",font=("Arial", 12)).pack(pady=5)
-ttk.Label(parent, text="5. Click Cam Data Chart To Show The Chart",font=("Arial", 12)).pack(pady=5)
+ttk.Label(parent, text=label_texts,font=("Arial", 12)).pack(pady=5)
 
 # Buttons
 tk.Button(parent, text="Back",font="sans 10 bold",bg="#ddd",width=12, command=root.destroy).pack(pady=15)
