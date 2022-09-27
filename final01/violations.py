bced09d67fb891256ffde45e53c62baa3a34d0e7
shehjad97 <shehjadcse@gmail.com>
2022-09-27 21:39:33 +0600

    final 2

--- final01/violations.py
+++ final01/violations.py
@@ -17,17 +17,19 @@ class Violations(tk.Tk):
         style.theme_use('clam')
 
         # Add a Treeview widget
-        tree = ttk.Treeview(self, column=("License Number", "Detected Violation"), show='headings', height=15)
+        tree = ttk.Treeview(self, column=("Serial", "License Number", "Detected Violation"), show='headings', height=15)
         tree.column("# 1", minwidth=0, width=300, anchor="center")
-        tree.heading("# 1", text="License Number")
+        tree.heading("# 1", text="Serial")
         tree.column("# 2", minwidth=0, width=300, anchor="center")
-        tree.heading("# 2", text="Detected Violation")
+        tree.heading("# 2", text="License Number")
+        tree.column("# 3", minwidth=0, width=300, anchor="center")
+        tree.heading("# 3", text="Detected Violation")
 
         with open('violations.json', 'r') as openfile:
             records = json.load(openfile)
 
         for record in records:
-            tree.insert('', 'end', text="1", values=(record['license'], record['violation']))
+            tree.insert('', 'end', text="1", values=(record['no'], record['license'], record['violation']))
 
         tree.pack()
 
